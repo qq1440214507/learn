61dcfc995516386a4e4ab821acbe2eb92c9c75d3
wangyang <1440214507@qq.com>
2023-03-07 21:27:36 +0800

    go:框架增加优雅关闭功能

--- go/web-framework/main.go
+++ go/web-framework/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"web-framework/framework"
 	"web-framework/framework/middleware"
 	_ "web-framework/framework/middleware"
@@ -17,5 +22,14 @@ func main() {
 		Handler: core,
 		Addr:    "127.0.0.1:8080",
 	}
-	server.ListenAndServe()
+	go func() {
+		server.ListenAndServe()
+	}()
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	<-quit
+	// 优雅关闭
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
 }
